70dc15a29f6aeb17dd7e146809b5b387ef808261
William Trevor Olson <wtolson@gmail.com>
2013-05-02 11:24:34 -0700

    Add python-and-mongo presentation to the build script.

--- fabfile.py
+++ fabfile.py
@@ -11,11 +11,23 @@ def create():
     if not os.path.exists(dirname):
         os.makedirs(dirname)
 
+def clean():
+    """Cleans up the build directory"""
+    local("rm -r output")
 
 def build():
     """Builds the presentation provided"""
+
+    # Setup build directory
+    local("mkdir -p output")
+    local("echo presentations.desertpy.com > output/CNAME")
+
+    # Fabric by godber
+    local("mkdir -p output/fabric-godber")
     local("landslide -i fabric-godber/presentation.rst -d output/fabric-godber/index.html")
 
+    # Python and MongoDB by wtolson
+    local("cp -r python-and-mongodb output/")
 
 def publish():
     """Publish the static content to Github Pages"""
