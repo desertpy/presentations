6cde956bc91c88473dc240f5e6df6dd4c28210d8
Austin Godber <godber@uberhip.com>
2017-04-26 18:00:11 -0700

    adding sockets talk

---
+++ godber-sockets-lightning-talk/client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Copied from https://pymotw.com/3/socket/tcp.html#echo-client
+
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 10000)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
+
+try:
+
+    # Send data
+    message = b'This is the message.  It will be repeated.'
+    print('sending {!r}'.format(message))
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print('received {!r}'.format(data))
+
+finally:
+    print('closing socket')
+    sock.close()
